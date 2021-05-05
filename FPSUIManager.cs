485628a26dbc310bf13c450a25b17217c5acac34
briancain <brianccain@gmail.com>
2021-05-04 22:52:19 -0700

    Update with cursor hovor ui

--- FPSUIManager.cs
+++ FPSUIManager.cs
@@ -1,8 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class FPSUIManager : MonoBehaviour {
   [Header("UI Sprites")]
   public Sprite cursor;
+  public Sprite cursorAim;
+
+  public GameObject cameraAim;
+  public TMP_Text objectNameHover;
+
+  void Start() {
+    objectNameHover.SetText("Default");
+  }
+
+  void Awake() {
+    // Assert cameraAim not nil
+    if (cameraAim == null) {
+      Debug.LogError("cameraAim UI canvas object must be set");
+    }
+  }
+
+  public void UpdateCursor(string name) {
+    objectNameHover.SetText(name);
+  }
 }
