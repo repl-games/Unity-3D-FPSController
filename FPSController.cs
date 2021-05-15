1a128f2f053d9e8745d3fd71ad1c6320fc252fb1
briancain <brianccain@gmail.com>
2021-05-15 15:41:37 -0700

    Add smooth FPS zoom on camera enable
    This commit adds a zoom camera effect that emulates a 16mm camera focal
    length and other properties. It includes a smooth zoom over time to act
    like the player "brought up" the camera to look through its lens.

--- FPSController.cs
+++ FPSController.cs
@@ -11,14 +11,20 @@ public class FPSController : MonoBehaviour {
 
     [Header("Player camera")]
     public Camera playerCamera;
+    public Sprite aimImage;
+    public Sprite targetImage;
+
+    public float lookSpeed = 2.0f;
+    public float cameraZoomLookSpeed = 0.5f;
+    public float aimSpeed = 0.75f;
+    public float lookXLimit = 45.0f;
 
     [Header("Player attributes")]
     public float walkingSpeed = 7.5f;
+    public float cameraToggleSpeed = 2f;
     public float runningSpeed = 11.5f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
-    public float lookSpeed = 2.0f;
-    public float lookXLimit = 45.0f;
 
     [Header("Options")]
     public bool canMove = true;
@@ -31,15 +37,20 @@ public class FPSController : MonoBehaviour {
 
     CharacterController characterController;
 
+    private bool cameraToggle = false;
+
     // Movement
     private Vector3 moveDirection = Vector3.zero;
     private float rotationX = 0;
 
+    // Camera Vars
+    private float zoomFOV = 20;
+    private float normalFOV = 60;
+    private float zoomIncreaseRate = 175f;
+
     // FPS Controller
     //
-
     private FPSUIManager uiManager;
-
 
     void Start() {
         characterController = GetComponent<CharacterController>();
@@ -52,7 +63,14 @@ public class FPSController : MonoBehaviour {
 
     void Update() {
       PlayerMove();
+      ToggleCamera();
     }
+
+    // Public Helpers
+    //
+
+    // Private Functions
+    //
 
     private void PlayerMove() {
         // We are grounded, so recalculate move direction based on axes
@@ -63,8 +81,19 @@ public class FPSController : MonoBehaviour {
         // Press c to toggle crouch
         bool isCrouching = Input.GetKey(KeyCode.C);
 
-        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        float curSpeedX = 0f;
+        float curSpeedY = 0f;
+        float playerSpeed = 0f;
+
+        if (canMove) {
+          if (isRunning) {
+            playerSpeed = runningSpeed;
+          } else {
+            playerSpeed = walkingSpeed;
+          }
+            curSpeedX = playerSpeed * Input.GetAxis("Vertical");
+            curSpeedY = playerSpeed * Input.GetAxis("Horizontal");
+        }
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
@@ -84,11 +113,48 @@ public class FPSController : MonoBehaviour {
         characterController.Move(moveDirection * Time.deltaTime);
 
         // Player and Camera rotation
+        float cameraMoveSpeed = lookSpeed;
+        if (cameraToggle) {
+          cameraMoveSpeed = cameraZoomLookSpeed;
+        }
+
         if (canMove) {
-            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX += -Input.GetAxis("Mouse Y") * cameraMoveSpeed;
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * cameraMoveSpeed, 0);
         }
+    }
+
+    private void ToggleCamera() {
+      if(Input.GetMouseButtonDown(1)) {
+        if (cameraToggle) {
+          DisableInGameCamera();
+        } else {
+          EnableInGameCamera();
+        }
+      }
+
+      // Smooth zoom
+      if (cameraToggle && playerCamera.fieldOfView >= zoomFOV) {
+        playerCamera.fieldOfView -= zoomIncreaseRate*Time.deltaTime;
+      } else if (!cameraToggle && playerCamera.fieldOfView <= normalFOV) {
+        playerCamera.fieldOfView += zoomIncreaseRate*Time.deltaTime;
+      }
+    }
+
+    IEnumerator WaitToggleCameraUI(bool enabled) {
+      yield return new WaitForSeconds(0.25f);
+      cameraUICanvas.SetActive(enabled);
+    }
+
+    private void EnableInGameCamera() {
+      StartCoroutine(WaitToggleCameraUI(true));
+      cameraToggle = true;
+    }
+
+    private void DisableInGameCamera() {
+      cameraUICanvas.SetActive(false); // disable right away
+      cameraToggle = false;
     }
 }
